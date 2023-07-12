69822db8575c9847867d97fbe1d4d64b51032e9f
akshay183 <sharmahoney398@gmail.com>
2023-07-12 19:17:18 +0530

    Listener added

--- src/main/java/com/kafkaproducer/KafkaListeners.java
+++ src/main/java/com/kafkaproducer/KafkaListeners.java
@@ -1,4 +1,17 @@
 package com.kafkaproducer;
 
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class KafkaListeners {
+
+    @KafkaListener(
+            topics = "kafkaExampleTopic",
+            groupId = "UUID100"
+    )
+    void listener(String data) {
+        System.out.println("Kafka listening" + data);
+    }
+
 }
